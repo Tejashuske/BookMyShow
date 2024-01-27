9117a5431457036600cee56cbd8472921933881e
Tejashuske <127583360+Tejashuske@users.noreply.github.com>
2024-01-27 18:14:10 +0000

    d8

---
+++ bookmyshow/src/pages/Movie.Page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+// HOC
+import MovielayoutHoc from "../layout/Movie.layout";
+
+
+const MoviePage = () => {
+  return <div>MoviePage</div>;
+};
+
+export default MovielayoutHoc(MoviePage)
